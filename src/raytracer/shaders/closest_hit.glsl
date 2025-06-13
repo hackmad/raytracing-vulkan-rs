a64ebedd22ab7ed21b5803d4d339f90fcb72193c
Ahmad Kabani <ahmadkabani@yahoo.com>
2025-06-13 13:06:47 -0400

    Refactor shader code for material scatter functions

--- src/raytracer/shaders/closest_hit.glsl
+++ src/raytracer/shaders/closest_hit.glsl
@@ -111,6 +111,71 @@ float schlickReflectance(float cosine, float refractionIndex) {
     return r0 + (1.0 - r0) * pow((1.0 - cosine), 5);
 }
 
+void scatterLambertianMaterial(uint materialIndex, HitRecord rec) {
+    if (materialIndex >= 0 && materialIndex < pc.lambertianMaterialCount) {
+        LambertianMaterial material = lambertianMaterial.values[materialIndex];
+        vec3 albedo = getMaterialPropertyValue(material.albedo, rec.meshVertex);
+
+        vec3 scatterDirection = rec.normal + randomUnitVec3(rayPayload.rngState);
+
+        // Catch degenerate scatter direction.
+        if (nearZero(scatterDirection)) {
+            scatterDirection = rec.normal;
+        }
+
+        rayPayload.attenuation = albedo;
+        rayPayload.isScattered = true;
+        rayPayload.scatteredRayDirection = scatterDirection;
+        rayPayload.scatteredRayOrigin = rec.meshVertex.position;
+    }
+}
+
+void scatterMetalMaterial(uint materialIndex, HitRecord rec) {
+    if (materialIndex >= 0 && materialIndex < pc.metalMaterialCount) {
+        MetalMaterial material = metalMaterial.values[materialIndex];
+        vec3 albedo = getMaterialPropertyValue(material.albedo, rec.meshVertex);
+        vec3 fuzz = getMaterialPropertyValue(material.fuzz, rec.meshVertex);
+
+        vec3 reflectedDirection = reflect(gl_WorldRayDirectionEXT, rec.normal);
+
+        vec3 scatteredDirection = normalize(reflectedDirection) +
+            (fuzz * randomUnitVec3(rayPayload.rngState));
+
+        rayPayload.attenuation = albedo;
+        rayPayload.isScattered = (dot(scatteredDirection, rec.normal) > 0);
+        rayPayload.scatteredRayDirection = scatteredDirection;
+        rayPayload.scatteredRayOrigin = rec.meshVertex.position;
+    }
+}
+
+void scatterDielectricMaterial(uint materialIndex, HitRecord rec) {
+    if (materialIndex >= 0 && materialIndex < pc.dielectricMaterialCount) {
+        DielectricMaterial dielectricMaterial = dielectricMaterial.values[materialIndex];
+        float refractionIndex = dielectricMaterial.refractionIndex;
+
+        vec3 attenuation = vec3(1.0);
+
+        float ri = rec.isFrontFace ? (1.0 / refractionIndex) : refractionIndex;
+
+        vec3 unitDirection = normalize(gl_WorldRayDirectionEXT);
+
+        float cosTheta = min(dot(-unitDirection, rec.normal), 1.0);
+        float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
+
+        bool cannotRefract = ri * sinTheta > 1.0; 
+        cannotRefract = cannotRefract || schlickReflectance(cosTheta, ri) > randomFloat(rayPayload.rngState);
+
+        vec3 refractedDirection = cannotRefract
+            ? reflect(unitDirection, rec.normal) // Total internal reflection.
+            : refract(unitDirection, rec.normal, ri);
+
+        rayPayload.attenuation = attenuation;
+        rayPayload.isScattered = true;
+        rayPayload.scatteredRayDirection = refractedDirection;
+        rayPayload.scatteredRayOrigin = rec.meshVertex.position;
+    }
+}
+
 void unpackInstanceMaterialAndScatter(const int instanceId, HitRecord rec) {
     Mesh mesh = mesh_data.values[instanceId];
     uint materialType = mesh.materialType;
@@ -118,68 +183,15 @@ void unpackInstanceMaterialAndScatter(const int instanceId, HitRecord rec) {
 
     switch (materialType) {
         case MAT_TYPE_LAMBERTIAN:
-            if (materialIndex >= 0 && materialIndex < pc.lambertianMaterialCount) {
-                LambertianMaterial lambertianMaterial = lambertianMaterial.values[materialIndex];
-                vec3 albedo = getMaterialPropertyValue(lambertianMaterial.albedo, rec.meshVertex);
-
-                vec3 scatterDirection = rec.normal + randomUnitVec3(rayPayload.rngState);
-
-                // Catch degenerate scatter direction.
-                if (nearZero(scatterDirection)) {
-                    scatterDirection = rec.normal;
-                }
-
-                rayPayload.attenuation = albedo;
-                rayPayload.isScattered = true;
-                rayPayload.scatteredRayDirection = scatterDirection;
-                rayPayload.scatteredRayOrigin = rec.meshVertex.position;
-            }
+            scatterLambertianMaterial(materialIndex, rec);
             break;
 
         case MAT_TYPE_METAL:
-            if (materialIndex >= 0 && materialIndex < pc.metalMaterialCount) {
-                MetalMaterial metalMaterial = metalMaterial.values[materialIndex];
-                vec3 albedo = getMaterialPropertyValue(metalMaterial.albedo, rec.meshVertex);
-                vec3 fuzz = getMaterialPropertyValue(metalMaterial.fuzz, rec.meshVertex);
-
-                vec3 reflectedDirection = reflect(gl_WorldRayDirectionEXT, rec.normal);
-
-                vec3 scatteredDirection = normalize(reflectedDirection) +
-                    (fuzz * randomUnitVec3(rayPayload.rngState));
-
-                rayPayload.attenuation = albedo;
-                rayPayload.isScattered = (dot(scatteredDirection, rec.normal) > 0);
-                rayPayload.scatteredRayDirection = scatteredDirection;
-                rayPayload.scatteredRayOrigin = rec.meshVertex.position;
-            }
+            scatterMetalMaterial(materialIndex, rec);
             break;
 
         case MAT_TYPE_DIELECTRIC:
-            if (materialIndex >= 0 && materialIndex < pc.dielectricMaterialCount) {
-                DielectricMaterial dielectricMaterial = dielectricMaterial.values[materialIndex];
-                float refractionIndex = dielectricMaterial.refractionIndex;
-
-                vec3 attenuation = vec3(1.0);
-
-                float ri = rec.isFrontFace ? (1.0 / refractionIndex) : refractionIndex;
-
-                vec3 unitDirection = normalize(gl_WorldRayDirectionEXT);
-
-                float cosTheta = min(dot(-unitDirection, rec.normal), 1.0);
-                float sinTheta = sqrt(1.0 - cosTheta * cosTheta);
-
-                bool cannotRefract = ri * sinTheta > 1.0; 
-                cannotRefract = cannotRefract || schlickReflectance(cosTheta, ri) > randomFloat(rayPayload.rngState);
-
-                vec3 refractedDirection = cannotRefract
-                    ? reflect(unitDirection, rec.normal) // Total internal reflection.
-                    : refract(unitDirection, rec.normal, ri);
-
-                rayPayload.attenuation = attenuation;
-                rayPayload.isScattered = true;
-                rayPayload.scatteredRayDirection = refractedDirection;
-                rayPayload.scatteredRayOrigin = rec.meshVertex.position;
-            }
+            scatterDielectricMaterial(materialIndex, rec);
             break;
     }
 }
