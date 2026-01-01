42324bf463895ee880eb3c0a1060f8db997b78c1
Ahmad Kabani <ahmadkabani@yahoo.com>
2026-01-01 18:00:11 -0500

    Replace transforms with individual option translate/rotate/scale
    This way we can compose final matrix in a way that is useful for
    animated transforms.

--- scene_file/src/instance.rs
+++ scene_file/src/instance.rs
@@ -5,46 +5,45 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "snake_case")]
 pub struct Instance {
     pub name: String,
-    pub transforms: Option<Vec<Transform>>,
+    pub transform: Option<Transform>,
 }
 
 impl Instance {
     /// Multiplies all transformation matrices in order to get final transformation for the
     /// object in world space.
     pub fn get_object_to_world_space_matrix(&self) -> Mat4 {
-        match &self.transforms {
-            None => Mat4::IDENTITY,
-            Some(transforms) => transforms.iter().fold(Mat4::IDENTITY, |acc, transform| {
-                acc.mul_mat4(&transform.to_matrix())
-            }),
-        }
+        self.transform
+            .as_ref()
+            .map_or(Mat4::IDENTITY, |t| t.to_matrix())
     }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
-pub enum Transform {
-    Translate([f32; 3]),
-    RotateX(f32),
-    RotateY(f32),
-    RotateZ(f32),
-    Scale([f32; 3]),
+pub struct Rotate {
+    pub axis: [f32; 3],
+    pub degrees: f32,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct Transform {
+    pub translate: Option<[f32; 3]>,
+    pub rotate: Option<Rotate>,
+    pub scale: Option<[f32; 3]>,
 }
 
 impl Transform {
     pub fn to_matrix(&self) -> Mat4 {
-        match self {
-            Self::Translate(d) => Mat4::from_translation(Vec3::new(d[0], d[1], d[2])),
-            Self::RotateX(degrees) => {
-                Mat4::from_axis_angle(Vec3::new(1.0, 0.0, 0.0), degrees.to_radians())
-            }
-            Self::RotateY(degrees) => {
-                Mat4::from_axis_angle(Vec3::new(0.0, 1.0, 0.0), degrees.to_radians())
-            }
-            Self::RotateZ(degrees) => {
-                Mat4::from_axis_angle(Vec3::new(0.0, 0.0, 1.0), degrees.to_radians())
-            }
-            Self::Scale(s) => Mat4::from_scale(Vec3::new(s[0], s[1], s[2])),
-        }
+        let t = self.translate.as_ref().map_or(Mat4::IDENTITY, |d| {
+            Mat4::from_translation(Vec3::new(d[0], d[1], d[2]))
+        });
+        let r = self.rotate.as_ref().map_or(Mat4::IDENTITY, |r| {
+            Mat4::from_axis_angle(r.axis.into(), r.degrees.to_radians())
+        });
+        let s = self.scale.as_ref().map_or(Mat4::IDENTITY, |s| {
+            Mat4::from_scale(Vec3::new(s[0], s[1], s[2]))
+        });
+        t.mul_mat4(&r).mul_mat4(&s)
     }
 }
